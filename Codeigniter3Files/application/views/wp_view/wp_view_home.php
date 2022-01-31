acdb6815b22bbea3745ece0255342c832736c91f
Pat_Gall <dread.knight02@gmail.com>
2022-01-31 20:42:35 +0800

    deleted unnecessary files

--- Codeigniter3Files/application/views/wp_view/wp_view_home.php
+++
@@ -1,8 +0,0 @@
-<h2>Home</h2>
-<?php  
-        if(!empty($success_arr)){ 
-            echo '<p class="status-msg success">'.$success_arr.'</p>'; 
-        }elseif(!empty($error_arr)){ 
-            echo '<p class="status-msg error">'.$error_arr.'</p>'; 
-        } 
-    ?>
